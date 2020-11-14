4e51cdbbee2c1ef2d6e455d7e9fe2e4a3941917c
V1V3 <rochelleayes@gmail.com>
2020-11-14 17:02:27 -0500

    first

--- app/src/main/java/com/example/mindfulness/MainActivity.java
+++ app/src/main/java/com/example/mindfulness/MainActivity.java
@@ -2,19 +2,46 @@ package com.example.mindfulness;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton waterButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("onCreate", "MainActivity created");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        waterButton = findViewById(R.id.water_tracker);
     }
 
+    public void onClickWater(View view)
+    {
+        Log.i("onClick", "User clicked water tracker button");
+        Intent intent = new Intent(MainActivity.this, WaterActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickMood(View view)
+    {
+        Log.i("onClick", "User clicked mood tracker button");
+        Intent intent = new Intent(MainActivity.this, MoodActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickMeditate(View view)
+    {
+        Log.i("onClick", "User clicked mood tracker button");
+        Intent intent = new Intent(MainActivity.this, MeditateActivity.class);
+        startActivity(intent);
+    }
 
     protected void onResume(){
         super.onResume();
