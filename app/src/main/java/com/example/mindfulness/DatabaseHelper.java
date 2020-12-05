604c6df3dd019c651ae102517f360e11a6d28445
ProfessorMuffinn <qbrimey@gmail.com>
2020-12-04 20:22:06 -0500

    jorunal adding more more

--- app/src/main/java/com/example/mindfulness/DatabaseHelper.java
+++ app/src/main/java/com/example/mindfulness/DatabaseHelper.java
@@ -10,7 +10,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
 
     public static final String DATABASE_NAME = "MindfulDatabase";
-    public static final int VERSION_NUM = 2;
+    public static final int VERSION_NUM = 3;
     public static String USER_ID = "UserID";
     public static String TABLE_NAME = "Accounts";
     public static String USERNAME = "UserName";
@@ -27,7 +27,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     //private static final String DATABASE_CREATE = "create table " + TABLE_NAME + "(" + USER_ID + " integer primary key autoincrement, " + USERNAME + " text not null);";
 
     public static final String DATABASE_CREATE = "create table " + TABLE_NAME + "(" + USER_ID + " integer primary key autoincrement, " + USERNAME + " text not null, " + PASSWORD + " text not null);";
-    public static final String DATABASE_CREATE_JOURNAL = "create table " + JOURNAL_TABLE_NAME + "(" + USER_ID + " integer primary key, " + DATE + " text not null, " + TEXTONE + " text not null, " + TEXTTWO + " text not null," + TEXTTHREE + " text not null);";
+    public static final String DATABASE_CREATE_JOURNAL = "create table " + JOURNAL_TABLE_NAME + "(" + USER_ID + " integer, " + DATE + " text not null, " + TEXTONE + " text not null, " + TEXTTWO + " text not null," + TEXTTHREE + " text not null);";
 
 
     public DatabaseHelper(Context ctx) {
@@ -45,6 +45,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion) {
         Log.i(ACTIVITY_NAME, "Calling onUpgrade, oldVersion=" + oldVersion + "newVersion=" + newVersion);
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + JOURNAL_TABLE_NAME);
         onCreate(db);
     }
 }
