f85e0a2e786c5487ca19ab11efa265aad2b34425
V1V3 <rochelleayes@gmail.com>
2020-12-09 21:25:44 -0500

    first

--- app/src/main/java/com/example/mindfulness/SleepActivity.java
+++ app/src/main/java/com/example/mindfulness/SleepActivity.java
@@ -8,14 +8,22 @@ import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.time.Duration;
+
+import java.time.LocalTime;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 import android.widget.TimePicker;
+import java.time.Duration;
 
 import java.util.Calendar;
 
-public class SleepActivity extends AppCompatActivity {
+public class SleepActivity extends AppCompatActivity
+{
 
     @SuppressLint("StaticFieldLeak")
     static TextView sleepTime;
@@ -23,23 +31,34 @@ public class SleepActivity extends AppCompatActivity {
     static TextView wakeTime;
     TextView label_sleepTime;
     TextView label_wakeTime;
-    TextView label_hoursOfSleep;
+    static TextView label_hoursOfSleep;
+    static TextView tot;
     static int sleepOrWake = -1;
+    static int sleepHour;
+    static int sleepMinute;
+    static LocalTime st;
+    static LocalTime wt;
+    static LocalTime tt;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep);
 
         sleepTime = findViewById(R.id.sleepTime);
         wakeTime = findViewById(R.id.wakeTime);
+        label_hoursOfSleep = findViewById(R.id.label_hoursOfSleep);
+        tot = findViewById(R.id.totalHrs);
     }
 
     public static class TimePickerFragment extends DialogFragment
-            implements TimePickerDialog.OnTimeSetListener {
+            implements TimePickerDialog.OnTimeSetListener
+    {
 
         @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
+        public Dialog onCreateDialog(Bundle savedInstanceState)
+        {
             // Use the current time as the default values for the picker
             final Calendar c = Calendar.getInstance();
             int hour = c.get(Calendar.HOUR_OF_DAY);
@@ -49,14 +68,39 @@ public class SleepActivity extends AppCompatActivity {
                     DateFormat.is24HourFormat(getActivity()));
         }
 
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute)
+        {
             if (sleepOrWake == 0)
             {
-                sleepTime.setText(hourOfDay + ":" + minute);
+                String sleepyTime = "";
+                if(hourOfDay>=0 && hourOfDay<10)
+                {
+                    sleepyTime = "0" + Integer.toString(hourOfDay) + ":" + Integer.toString(minute);
+                }
+                else
+                {
+                    sleepyTime = Integer.toString(hourOfDay) + ":" + Integer.toString(minute);
+                }
+                st = LocalTime.parse(sleepyTime);
+                sleepTime.setText(sleepyTime);
+                sleepHour = hourOfDay;
+                sleepMinute = minute;
             }
             else if(sleepOrWake == 1)
             {
-                wakeTime.setText(hourOfDay + ":" + minute);
+                String wakeyTime = "";
+                if(hourOfDay>=0 && hourOfDay<10)
+                {
+                    wakeyTime = "0" + Integer.toString(hourOfDay) + ":" + Integer.toString(minute);
+                }
+                else
+                {
+                    wakeyTime = Integer.toString(hourOfDay) + ":" + Integer.toString(minute);
+                }
+                wakeTime.setText(wakeyTime);
+                wt = LocalTime.parse(wakeyTime);
+                tt = wt.minus(Duration.ofHours(st.getHour()));
+                tot.setText(tt.toString());
             }
         }
     }
